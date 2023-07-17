af1e47a6c1de279a91c863d4839c11305d9f25a5
podpison <au25557@yandex.ru>
2023-07-17 17:36:35 +0500

    favicon

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
 import Header from '@/ui/header'
 import './globals.scss'
 import { Montserrat } from 'next/font/google'
+import { Metadata } from 'next'
 
 const montserrat = Montserrat({
   subsets: ['latin'],
@@ -15,6 +16,10 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={montserrat.className}>
+        <link rel="apple-touch-icon" sizes="180x180" href="./../public/favicon/apple-touch-icon.png" />
+        <link rel="icon" type="image/png" sizes="32x32" href="./../public/favicon/favicon-32x32.png" />
+        <link rel="icon" type="image/png" sizes="16x16" href="./../public/favicon/favicon-16x16.png" />
+        <link rel="manifest" href="./../public/favicon/site.webmanifest" />
         <Header />
         {children}
       </body>
