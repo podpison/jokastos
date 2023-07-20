2de4b4c46a4a7f56c3478312b1b5335745d442ba
podpison <au25557@yandex.ru>
2023-07-20 16:43:39 +0500

    nav ui bug fix

--- ui/header/nav/index.tsx
+++ ui/header/nav/index.tsx
@@ -10,8 +10,8 @@ type Props = {
 const Nav: FC<Props> = ({ pages, className }) => {
   let Items = pages.map((p, index) => <Item page={p} key={index} />);
 
-  return <nav className={cn('', className)}>
-    <ul className='justify-center flex-grow gap-x-2 md:flex'>
+  return <nav>
+    <ul className={cn('justify-center flex-grow gap-x-2 md:flex', className)}>
       {Items}
     </ul>
   </nav>
